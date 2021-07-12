da24bc6698145abb0acb6e2bca11754bbbf3d8ab
Balázs Burján <47102427+bigbali@users.noreply.github.com>
2021-07-12 23:30:09 +0300

    Sleep Magic (allow adding multiple product variations)

--- src/redux/reducer/partialReducers/cartReducer.js
+++ src/redux/reducer/partialReducers/cartReducer.js
@@ -2,6 +2,31 @@ import * as actions from "../../actions/types";
 import initialState from "../../initialState";
 
 const cartReducer = (state = null, action) => {
+
+    /*  I could barely keep my eyes open, that's how tired I was...
+        and yet I solved the problem like a magician, with magic.
+        I don't even know how it happened, it just did.
+        Like, I just looked at my screen and 5 minutes later I had a
+        solution without looking anything up.
+        Now I'm making a sleepy pikachu face. Imagine how that looks.
+        :>
+    */
+    const getX = (x, variation) => {
+
+        // TODO: check if variation already exist, and if so don't add product
+        // TODO: documentation -> comments... comments everywhere; Refactor, also!
+        if(x){
+            return [
+                ...x["variations"],
+                variation
+            ]
+        }
+        else {
+            return [
+                variation
+            ]
+        }
+    }
     switch (action.type){
         case actions.CART_ADD:
 
@@ -14,16 +39,13 @@ const cartReducer = (state = null, action) => {
                 }
             });
 
-            console.log(state[action.payload.productId])
+            let x = state[action.payload.productId];
 
-            
-            
+            console.log(state)
+
             return {
                 [action.payload.productId]: {
-                    variations: [
-                        //...state[action.payload.productId].variations,
-                        variation
-                    ]
+                    variations: getX(state[action.payload.productId], variation)
                 },
             }
         case actions.CART_REMOVE:
